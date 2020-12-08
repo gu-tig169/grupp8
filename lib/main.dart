a9b7851867b8974b818d3aa9b6311fa95f20e34f
Erik <frisenstamerik@gmail.com>
2020-12-08 15:52:50 +0100

    ui-work

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
 
-void main() {}
+import 'welcome_screen.dart';
+
+void main() {
+  //hämtar listan från API varje gång appen startas
+
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Widget background() {
+    return Image(image: AssetImage('images/framsida.png'));
+  }
+}
