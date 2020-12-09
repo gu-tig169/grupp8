3cf766936665ff3606ca4f18592cd456bf89a45e
EmilioSvensson <gussveembd@student.gu.se>
2020-12-09 11:02:49 +0100

    buttons

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -3,14 +3,49 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: background(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        body: background(context)
+      )
     );
   }
 
-  Widget background() {
-    return
-        Image(image: AssetImage('assets/images/framsida.png'));
-        
+  Widget background(context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 1,
+      height: MediaQuery.of(context).size.height * 1,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/framsida.png"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              registerButton(),
+              logInButton(),
+            ],
+          ),
+      );
+    
+  }
+
+  Widget registerButton() {
+    return FlatButton.icon(
+      onPressed: () {},
+      label: Text("REGISTER"),
+      icon: Icon(Icons.radio_button_on_outlined),
+    );
+  }
+    Widget logInButton() {
+    return FlatButton.icon(
+      onPressed: () {},
+      label: Text("LOGIN"),
+      icon: Icon(Icons.radio_button_on_outlined),
+    );
   }
 }
