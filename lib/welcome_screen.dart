01d9c21a805e45aeb78dee601204c61b1901b4ae
EmilioSvensson <gussveembd@student.gu.se>
2020-12-09 11:30:06 +0100

    welcompage done

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -4,48 +4,58 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.tealAccent,
-        body: background(context)
-      )
-    );
+        home: Scaffold(
+            backgroundColor: Colors.tealAccent, body: background(context)));
   }
 
   Widget background(context) {
     return Container(
       width: MediaQuery.of(context).size.width * 1,
       height: MediaQuery.of(context).size.height * 1,
-        decoration: BoxDecoration(
+      decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/images/framsida.png"),
-            fit: BoxFit.cover
-          )
-        ),
+              image: AssetImage("assets/images/framsida.png"),
+              fit: BoxFit.cover)),
+      child: Container(
+        margin: const EdgeInsets.all(0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              registerButton(),
-              logInButton(),
-            ],
-          ),
-      );
-    
+          children: [
+            logInButton(),
+            registerButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget logInButton() {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: 15,
+      ),
+      child: Transform.scale(
+        scale: 6,
+        child: IconButton(
+            icon: Image.asset("assets/images/logInButton.png"),
+            onPressed: () {
+            }),
+      ),
+    );
   }
 
   Widget registerButton() {
-    return FlatButton.icon(
-      onPressed: () {},
-      label: Text("REGISTER"),
-      icon: Icon(Icons.radio_button_on_outlined),
-    );
-  }
-    Widget logInButton() {
-    return FlatButton.icon(
-      onPressed: () {},
-      label: Text("LOGIN"),
-      icon: Icon(Icons.radio_button_on_outlined),
+    return Padding(
+      padding: EdgeInsets.only(bottom: 15),
+      child: Transform.scale(
+        scale: 6,
+        child: IconButton(
+            icon: Image.asset("assets/images/registerButton.png"),
+            onPressed: () {
+            }),
+      ),
     );
   }
 }
