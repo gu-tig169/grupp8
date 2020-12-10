1c6a8d31812a7126b3c9334e1a0cdb482c4842dc
Carl Engman <carlengman@hotmail.com>
2020-12-10 10:03:21 +0100

    Background color for homeScreen set

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,6 +8,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color.fromARGB(255, 86, 75, 83),
+    );
   }
 }
