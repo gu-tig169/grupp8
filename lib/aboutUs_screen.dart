95e05948dc2371ce129600bb7307eb0850545cda
imschafhauser <gusschais@student.gu.se>
2020-12-14 15:45:56 +0100

    Informationsvy WIP

--- lib/aboutUs_screen.dart
+++ lib/aboutUs_screen.dart
@@ -4,29 +4,63 @@ class AboutUs extends StatefulWidget {
   @override
   _AboutUsState createState() => _AboutUsState();
 }
-
 class _AboutUsState extends State<AboutUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            
-             color: Color.fromARGB(255, 86, 75, 83),
-             child: Center(
-
-               child: Text('Det är vi som är projektgrupp 8')
-             )
-  
+      backgroundColor: Color.fromARGB(255, 86, 75, 83), 
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent, 
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {}
           ),
-          Positioned(
-            child: AppBar(
-              backgroundColor: Colors.transparent,
+        ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                ),
+              ],
             ),
-          ),
-        ]
-      )
-    );
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "About Us",
+                    style: TextStyle(
+                      fontSize: 35,
+                      letterSpacing: 1.5,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,                      
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  width: MediaQuery.of(context).size.width/2,
+                  height: MediaQuery.of(context).size.width/2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+              // image: DecorationImage(
+              //  fit: BoxFit.cover,
+              //    image: AssetImage('images/grupp8.png'),
+                    ),
+                  ),
+             //  ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
   }
-}
+
