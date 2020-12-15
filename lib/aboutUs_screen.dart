2eb4c8d5fb86d3bdccc13bcc2bca0cc5a9b8830b
imschafhauser <gusschais@student.gu.se>
2020-12-15 12:31:10 +0100

    RichText och logOutButton

--- lib/aboutUs_screen.dart
+++ lib/aboutUs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'welcome_view.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -23,8 +24,7 @@ class _AboutUsState extends State<AboutUs> {
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Container(
-                ),
+                Container(),
               ],
             ),
             Column(
@@ -33,7 +33,7 @@ class _AboutUsState extends State<AboutUs> {
                 Padding(
                   padding: EdgeInsets.all(20),
                   child: Text(
-                    "About Us",
+                    "GRUPP 8",
                     style: TextStyle(
                       fontSize: 35,
                       letterSpacing: 1.5,
@@ -50,12 +50,14 @@ class _AboutUsState extends State<AboutUs> {
                   border: Border.all(color: Colors.white, width: 5),
                   shape: BoxShape.circle,
                   color: Colors.white,
-              // image: DecorationImage(
-              //  fit: BoxFit.cover,
-              //    image: AssetImage('images/grupp8.png'),
+                 image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/chewy1.jpg'),
                     ),
                   ),
-             //  ),
+               ),
+              _aboutParagraph(),
+              logoutButtonBody(context),
               ],
             ),
           ],
@@ -64,3 +66,59 @@ class _AboutUsState extends State<AboutUs> {
     }
   }
 
+// Textfältet för vår about us
+
+Widget _aboutParagraph() {
+return Container(
+    child: RichText(
+    text: TextSpan(
+     children: <TextSpan>[
+      TextSpan(
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ',
+         style: TextStyle(
+          fontSize: 18.0,
+          letterSpacing: 1.5,
+        ),
+       ),
+                     // TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
+                     // TextSpan(text: ' Hur får man en paragraf'),
+    ],
+   ),
+ ),
+ );
+}
+
+
+// Detta använder samma kod som i welcome_view - kan det göras till en mer universiell widget med 
+// texten definierad per button istället för separata knapp-widgets?
+  Widget logoutButtonBody(context) {
+    return Container(
+      child: SizedBox(
+        width: 185,
+        height: 45,
+        child: RaisedButton(
+          elevation: 7.0,
+          textColor: Color.fromARGB(255, 224, 234, 255),
+          child: Text(
+            "Log out",
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+          ),
+          color: Color.fromARGB(255, 107, 90, 100),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40.0),
+              side: BorderSide(color: Color.fromARGB(255, 86, 75, 83))),
+          onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => (WelcomeView())),
+                    );
+                  },
+        ),
+      ),
+    );
+  }
+
+Widget appVerText() {
+  return Container(
+  );
+}
