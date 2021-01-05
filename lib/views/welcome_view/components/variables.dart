ace033fb9bb72b4416f130dc05828b70b4ed0230
imschafhauser <gusschais@student.gu.se>
2021-01-05 11:30:56 +0100

    Broke out welcome view widgets

---
+++ lib/views/welcome_view/components/variables.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+// Variables for slidingup panel
+
+PanelController pc = new PanelController();
+FocusNode focusNodeUsername = FocusNode();
+String hintTextUsername = 'Username';
+
+FocusNode focusNodePassword = FocusNode();
+String hintTextPassword = 'Password';
+
+final TextEditingController passwordInput = new TextEditingController();
+final TextEditingController usernameInput = new TextEditingController();
